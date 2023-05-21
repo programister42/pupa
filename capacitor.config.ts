e3339af327dc153d2ed35897c7d9d6ee9b0b7440
Taras Tereschenko <taras.tereschenko@sap.com>
2023-05-21 17:11:53 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.pupa',
+  appName: 'pupa',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
